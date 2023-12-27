00e8059609cd4997d3db4b8a45173f9eb58934ac
mitrajani506 <rmit9542@gmail.com>
2023-12-27 18:12:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Props from './commponet/Props';
+
+function App() {
+  return (
+    <div className="App">
+      <Props first="meet" last="rajani" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/commponet/Props.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Props = (props) => {
+  return (
+    <div>
+      <h1>My fist name is {props.first} and last name is {props.last}</h1>
+    </div>
+  )
+}
+
+export default Props
